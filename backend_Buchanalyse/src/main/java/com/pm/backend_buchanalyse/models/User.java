089d70bc2e4c0a156c682b9cb1fb0ffbb998931e
Gildas,Leodux <inf4453@hs-worms.de>
2026-04-06 17:28:18 +0200

    UserController,UserService und UserRepository

--- backend_Buchanalyse/src/main/java/com/pm/backend_buchanalyse/models/User.java
+++ backend_Buchanalyse/src/main/java/com/pm/backend_buchanalyse/models/User.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String id;
+    private int id;
     @Column(unique = true,nullable = false)
     private String email;
     @Column(nullable = false)
